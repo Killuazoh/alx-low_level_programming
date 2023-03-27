fe2a292044f64b00e627833982c571f73475bff1
Killuazoh <echabli.abdelilah@gmail.com>
2023-03-27 14:13:27 -0700

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,12 +5,12 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, i;
+	int i;
 
-	while (s[i++])
-		len++;
+	for (i = 0; s[i] != '\0'; i++)
+		;
 
-	for (i = len - 1; i >= 0; i--)
+	for (i--; i >= 0; i--)
 		_putchar(s[i]);
 
 	_putchar('\n');
