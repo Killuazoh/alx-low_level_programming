b9ec45c7d82740e6063c53e8ecce9e4aece7fa7f
Killuazoh <echabli.abdelilah@gmail.com>
2023-03-30 10:41:50 -0700

    103

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,66 +1,35 @@
 /**
- * infinite_add - Add up two numbers stored in given char arrays
- * @n1: The first number
- * @n2: The second number
- * @r: Pointer to the buffer to store result
- * @size_r: The size of the buffer
+ * infinite_add - Adds two numbers represented as strings
+ * @n1: First number to add
+ * @n2: Second number to add
+ * @r: Buffer to store the result
+ * @size_r: Size of the buffer
  *
- * Return: 0 if buffer too small to store result, else return pointer to buffer
+ * Return: Pointer to the result, or 0 if the result cannot be stored in r
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int l1, l2, tmpl, rl, i, sum, num1, num2, carry;
-	char tmp[10000];
+    int len1 = strlen(n1);
+    int len2 = strlen(n2);
+    int carry = 0;
+    int i, j, k;
 
-	rl = i = l1 = l2 = sum = num1 = num2 = carry = 0;
-	while (n1[l1] != '\0')
-		l1++;
-	while (n2[l2] != '\0')
-		l2++;
-	if (l1 + 2 > size_r || l2 + 2 > size_r)
-		return (0);
-	l1--;
-	l2--;
-	while (i <= l1 || i <= l2)
-	{
-		num1 = num2 = 0;
-		if (i <= l1)
-			num1 = n1[l1 - i] - '0';
-		if (i <= l2 && (l2 - i) >= 0)
-			num2 = n2[l2 - i] - '0';
-		sum = num1 + num2 + carry;
-		if (sum >= 10)
-		{
-			carry = 1;
-			sum -= 10;
-		}
-		else
-			carry = 0;
-		r[i] = sum + '0';
-		i++;
-		rl++;
-	}
-	if (carry > 0)
-	{
-		r[i] = carry + '0';
-		r[i + 1] = '\0';
-	}
-	i = tmpl = 0;
-	while (i <= rl)
-	{
-		tmp[i] = r[rl - i];
-		tmpl++;
-		i++;
-	}
-	i = 0;
-	while (i < tmpl)
-	{
-		if (r[i] == '\0')
-		{
-			break;
-		}
-		r[i] = tmp[i];
-		i++;
-	}
-	return (r);
+    if (len1 + len2 > size_r - 1)
+        return 0;
+
+    for (i = len1 - 1, j = len2 - 1, k = size_r - 1; i >= 0 || j >= 0; i--, j--, k--) {
+        int d1 = (i >= 0) ? n1[i] - '0' : 0;
+        int d2 = (j >= 0) ? n2[j] - '0' : 0;
+        int sum = d1 + d2 + carry;
+        r[k] = (sum % 10) + '0';
+        carry = sum / 10;
+    }
+
+    if (carry == 1 && k >= 0)
+        r[k--] = '1';
+
+    if (k < 0 || (k == 0 && r[k] == 0))
+        return 0;
+
+    return &r[k];
 }
